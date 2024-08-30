ca06d2c257c789625342fc0cb4f271b81762c1d0
Nicolás Tamargo <reosarevok@metabrainz.org>
2024-08-30 11:27:29 +0200

    MBS-13534: Show release status in SetCoverArt
    This would be useful to avoid selecting bootleg artwork, for example,
    if official artwork exists.

--- lib/MusicBrainz/Server/Controller/ReleaseGroup.pm
+++ lib/MusicBrainz/Server/Controller/ReleaseGroup.pm
@@ -182,6 +182,7 @@ sub set_cover_art : Chained('load') PathPart('set-cover-art') Args(0) Edit
         $entity->id);
     $c->model('Release')->load_related_info(@$releases);
     $c->model('Release')->load_meta(@$releases);
+    $c->model('ReleaseStatus')->load(@$releases);
     $c->model('ArtistCredit')->load(@$releases);
 
     my @non_darkened_releases = grep { $_->may_have_cover_art } @$releases;
